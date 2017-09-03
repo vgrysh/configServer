e012038dd127a03903c0af37f41b5cbabbb165da
vgrysh <vvov4ikk@rambler.ru>
2017-09-03 23:15:12 +0300

    init

---
+++ src/main/java/com/logeek/night/scc/SccApplication.java
@@ -0,0 +1,12 @@
+package com.logeek.night.scc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccApplication.class, args);
+	}
+}
